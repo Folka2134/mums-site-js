98185d724490b36a186bdb1c2529f51a51592b63
Folka2134 <folka2134@gmail.com>
2022-12-08 11:21:28 +0000

    impletemeted homeRoute

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require("express");
 
+// local Modules
+const homeRoutes = require("./routes/homeRoutes");
+
 // Express setup
 const app = express();
 const PORT = 5001;
-
-app.get("/", (req, res) => {
-  console.log("working");
-});
 
 // Middleware
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// routes
+app.use("/", homeRoutes);
 
 app.listen(PORT, () => {
   console.log(`server running on PORT:${PORT}`);
