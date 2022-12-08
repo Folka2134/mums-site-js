56f6346f1e64440378845cc57e0f4a161993e724
Folka2134 <folka2134@gmail.com>
2022-12-08 11:15:03 +0000

    added middleware

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
   console.log("working");
 });
 
+// Middleware
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`server running on PORT:${PORT}`);
 });
