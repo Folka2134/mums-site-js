4a41d5e49b8ac26dcffb63cc3a03b54af011f3df
Folka2134 <folka2134@gmail.com>
2022-12-14 10:48:21 +0000

    fixed package page

--- data.js
+++ data.js
@@ -12,7 +12,7 @@ const packageData = [
     title: "package2",
     mini_desc:
       "Short description about package2... Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo quis corrupti assumenda aspernatur, rerum quo quos laborum at esse eveniet neque praesentium voluptate minuseius?",
-    disc: "Main description about package2... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum beatae natus aspernatur maxime incidunt reprehenderit, reiciendis non? Amet dolorem facere labore, dolore minus reprehenderit officiis quaerat, minima ipsa recusandae similique repellendus pariatur magnam itaque aliquid ea praesentium rem repellat ullam. Impedit, molestias est! Pariatur, perferendis illum dignissimos praesentium, fugit provident exercitationem quis amet hic esse sequi blanditiis molestias, eligendi tenetur tempore modi excepturi debitis possimus! Rerum, accusantium architecto quidem dolor officiis similique magni suscipit nobis",
+    disc: "Main description about package2... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum beatae natus aspernatur maxime incidunt reprehenderit, reiciendis non? Amet dolorem facere labore, dolore minus reprehenderit officiis quaerat, minima ipsa recusandae similique repellendus pariatur magnam itaque aliquid ",
     image: "fa-clipboard",
   },
   {
